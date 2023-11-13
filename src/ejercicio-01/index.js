fa8bf7efc108402ca40770f270949637862ddde3
epolov <epolov@gmail.com>
2023-11-13 20:41:46 +0100

    solucion ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,5 @@
 // Escribe tu código aquí:
+
+const numeros = [2,4,5,6,7]
+const numerosAlCuadrado = numeros.map((numero) => numero * numero)
+console.log('Numeros al cuadrado: ', numerosAlCuadrado)
