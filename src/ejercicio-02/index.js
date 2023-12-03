404770914b86577821664c6597cfe2f4586d1e65
epolov <epolov@gmail.com>
2023-12-03 20:07:47 +0100

    Ejercicio02 - Arrow functions & this

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,19 @@
-// Escribe tu código aquí:
+// Escribe tu código aquí: EJERCICIO 02
+
+const persona = {
+    name: 'Eva',
+    greet: function () {
+        console.log(`Hola soy, ${this.name}`)
+    }
+}
+
+persona.greet()
+
+const persona2 = {
+    name: 'Manuel',
+    greet: () => {
+        console.log(`Hola soy ${this.name}`)
+    }
+}
+
+persona2.greet()
